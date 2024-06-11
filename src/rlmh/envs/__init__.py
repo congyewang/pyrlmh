0af65d1db2c47a0248a5884024c158776fc8de7d
Congye Wang <congye.wang@outlook.com>
2024-06-11 13:50:18 +0100

    Add gym register

--- src/rlmh/envs/__init__.py
+++ src/rlmh/envs/__init__.py
@@ -1,4 +1,11 @@
+from gymnasium.envs.registration import register
+
 from ._env import RLMHEnv
+
+register(
+    id="RLMHEnv-v0",
+    entry_point="src.rlmcmc.envs._env:RLMHEnv",
+)
 
 
 __all__ = ["RLMHEnv"]
